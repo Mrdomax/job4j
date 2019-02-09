95f7738890db4093705e1213ce33088da11d9ef2
Mrdomax <kasian.job@gmail.com>
2019-02-09 22:50:12 +0300

    1.3.1 Maven Plugins[#111757]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,14 +1,14 @@
 package ru.job4j.calculate;
 /**
 * Class Класс для вычисления арифметических операций + - * /.
-* @autor Semenchenko
+* @author Semenchenko
 * @since 09.02.2019
 * @version 1
 */
 public class Calculate {
 /** 
 * Конструктор, вывод строки в консоль
-* @param arg - arg.
+* @param args - arg.
 */	
 public static void main(String[] args){
 System.out.println("Hello World");
