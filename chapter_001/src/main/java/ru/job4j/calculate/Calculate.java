6793afad0c6e9b69f821ee032d77b89be0cf1fa7
Mrdomax <kasian.job@gmail.com>
2019-02-09 21:35:57 +0300

    1.3. JavaDoc. Code convention[#111760]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,15 @@
 package ru.job4j.calculate;
+/**
+* Class Класс для вычисления арифметических операций + - * /.
+* @autor Semenchenko
+* @since 09.02.2019
+* @version 1
+*/
 public class Calculate {
+/** 
+* Конструктор, вывод строки в консоль
+* @param arg - arg.
+*/	
 public static void main(String[] args){
 System.out.println("Hello World");
 }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Kasian Semenchenko (kasian.job@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
