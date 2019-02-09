fbc95c0c569cc36b9886f06e86cd5133ac6deceb
Mrdomax <kasian.job@gmail.com>
2019-02-09 23:18:19 +0300

    1.4. JUnit[#111761]

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Kasian Semenchenklo (kasian.job@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+public class CalculateTest {
+/**
+* Test echo.
+*/
+@Test
+public void whenTakeNameThenThreeEchoPlusName() {
+    String input = "Kasian Semenchenklo";
+    String expect = "Echo, echo, echo : Kasian Semenchenklo"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+}
+ 
+}
