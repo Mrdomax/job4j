3559718f7c9fb2536378554717471d387755a696
Mrdomax <kasian.job@gmail.com>
2019-02-10 15:37:48 +0300

    2.4. Использование Git в IDEA[#111717]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,25 +1,25 @@
 package ru.job4j.calculate;
 /**
-* Class Класс для вычисления арифметических операций + - * /.
-* @author Semenchenko
-* @since 09.02.2019
-* @version 1
-*/
+ * Class Класс для вычисления арифметических операций + - * /.
+ * @author Semenchenko
+ * @since 09.02.2019
+ * @version 1
+ */
 public class Calculate {
-/** 
-* Конструктор, вывод строки в консоль
-* @param args - arg.
-*/	
-public static void main(String[] args) {
-System.out.println("Hello World");
-}
+    /**
+     * Конструктор, вывод строки в консоль
+     * @param args - arg.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 
-/**
-* Method echo.
-* @param name Your name.
-* @return Echo plus your name.
-*/
-public String echo(String name) {
-    return "Echo, echo, echo : " + name;
-}
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -5,23 +5,23 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Kasian Semenchenklo (kasian.job@gmail.com)
-* @version $Id$
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author Kasian Semenchenklo (kasian.job@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class CalculateTest {
-/**
-* Test echo.
-*/
-@Test
-public void whenTakeNameThenThreeEchoPlusName() {
-    String input = "Kasian Semenchenklo";
-    String expect = "Echo, echo, echo : Kasian Semenchenklo"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
- 
+    /**
+     * Test echo.
+     */
+    @Test
+    public void whenTakeNameThenThreeEchoPlusName() {
+        String input = "Kasian Semenchenklo";
+        String expect = "Echo, echo, echo : Kasian Semenchenklo";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
+
 }
