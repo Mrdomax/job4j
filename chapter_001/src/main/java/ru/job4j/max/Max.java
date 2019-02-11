5dde1eae5921d94c81bbd1ee0adb08beebf13d17
Mrdomax <kasian.job@gmail.com>
2019-02-11 11:28:45 +0300

    4.2. Максимум из двух чисел[#111724]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,18 @@
+package ru.job4j.max;
+/**
+ * @author Semenchenko
+ * @version $Id$
+ * @since 0.1
+ */
+public class Max {
+    /**
+     * Metod
+     * @param first
+     * @param second
+     * @return возвращает большее из двух
+     */
+    public int max(int first, int second) {
+        return first > second ? first : second;
+    }
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.max;
+/**
+ * @author Semenchenko
+ * @version $Id$
+ * @since 0.1
+ */
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MaxTest {
+    @Test
+    public void whenFirstLessSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(1, 2);
+        assertThat(result, is(2));
+    }
+}
