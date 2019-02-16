d80c1282fd69a28c705fa78d90f1b173eb278b23
Mrdomax <kasian.job@gmail.com>
2019-02-17 00:44:22 +0300

    6.3. исправил 2

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -9,10 +9,10 @@ public class Check {
      * @return
      */
     public boolean mono(boolean[] data) {
-        boolean result = false;
+        boolean result = true;
         for (int i = 0; i < data.length; ++i) {
-            if( data [0] != data [i]) { result = false; break;}
-            else result = true;
+            if( data [0] != data [i]) { result = false; break; }
+
         }
         return result;
     }
