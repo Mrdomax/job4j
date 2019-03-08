63856b21e337d55f99ee5392baffc7d0186a7ede
Mrdomax <kasian.job@gmail.com>
2019-03-08 18:08:56 +0300

    Испавил

--- chapter_002/src/main/java/ru/job4j/pseudo/Paint.java
+++ chapter_002/src/main/java/ru/job4j/pseudo/Paint.java
@@ -7,7 +7,7 @@ package ru.job4j.pseudo;
  */
 public class Paint {
     public void draw(Shape shape) {
-        System.out.println(shape.draw());
+        System.out.print(shape.draw());
     }
 
     public static void main(String[] args) {
