cb2375ee3641383bce6d04b21cea601fa4ef8f7c
Mrdomax <kasian.job@gmail.com>
2019-03-11 01:59:55 +0300

    Исправил.

---
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,171 @@
+package ru.job4j.tracker;
+
+/**
+ * @version $Id$
+ * @since 0.1
+ */
+public class StartUI {
+    /**
+     * Константа меню для добавления новой заявки.
+     */
+    private static final String ADD = "0";
+
+    private static final String SHOW = "1";
+
+    private static final String EDIT = "2";
+
+    private static final String DELETE = "3";
+
+    private static final String FINDID = "4";
+
+    private static final String FINDNAME = "5";
+
+
+
+    /**
+     * Константа для выхода из цикла.
+     */
+    private static final String EXIT = "6";
+    /**
+     * Получение данных от пользователя.
+     */
+    private final Input input;
+
+    /**
+     * Хранилище заявок.
+     */
+    private final Tracker tracker;
+
+
+    /**
+     * Конструтор инициализирующий поля.
+     * @param input ввод данных.
+     * @param tracker хранилище заявок.
+     */
+    public StartUI(Input input, Tracker tracker) {
+        this.input = input;
+        this.tracker = tracker;
+    }
+
+
+    /**
+     * Основой цикл программы.
+     */
+    public void init() {
+        boolean exit = false;
+        while (!exit) {
+            this.showMenu();
+            String answer = this.input.ask("Введите пункт меню : ");
+            if (ADD.equals(answer)) {
+                //добавление заявки вынесено в отдельный метод.
+                this.createItem();
+           } else if (SHOW.equals(answer)) {
+                this.showAll();
+//             Добавить остальные действия системы по меню.
+            } else if (EDIT.equals(answer)) {
+                this.editItem();
+//             Добавить остальные действия системы по меню.
+            } else if (DELETE.equals(answer)) {
+                this.deleteItem();
+            } else if (FINDID.equals(answer)) {
+                this.findId();
+            } else if (FINDNAME.equals(answer)) {
+                this.findName();
+            } else if (EXIT.equals(answer)) {
+                exit = true;
+            }
+        }
+    }
+
+    /**
+     * Метод реализует добавленяи новый заявки в хранилище.
+     */
+    private void createItem() {
+        System.out.println("------------ Добавление новой заявки --------------");
+        String name = this.input.ask("Введите имя заявки :");
+        String desc = this.input.ask("Введите описание заявки :");
+        Item item = new Item(name, desc);
+        this.tracker.add(item);
+        System.out.println("------------ Новая заявка с getId : " + item.getId() + " -----------");
+    }
+
+    private void showAll() {
+        System.out.println();
+        System.out.println();
+        System.out.println("------------ Список всех заявок --------------");
+        Item[] items = tracker.findAll();
+        for (Item item : items) {
+            System.out.println("Имя заявки: " + item.getName());
+            System.out.println("Описание заявки: " + item.getDesc());
+            System.out.println("Id заявки: " + item.getId());
+            System.out.println("----------------------------------------------");
+        }
+        System.out.println();
+        System.out.println();
+    }
+
+    private void editItem() {
+        System.out.println("------------ Редактирование заявки --------------");
+        String id = this.input.ask("Введите id заявки :");
+        String name = this.input.ask("Введите новое имя заявки :");
+        String desc = this.input.ask("Введите новое описание заявки :");
+        Item item = new Item(name, desc, id);
+        if (tracker.replace(id, item)) {
+            System.out.println("Ваша заявка отредактирована");
+        } else {
+            System.out.println("Ваша заявка не отредактирована");
+        }
+
+    }
+
+    private void deleteItem() {
+        System.out.println("------------ Удаление заявки --------------");
+        String id = this.input.ask("Введите id заявки :");
+        if (tracker.delete(id)) {
+            System.out.println("Ваша заявка удалена");
+        } else {
+            System.out.println("Ваша заявка не удалена");
+        }
+    }
+
+    private void findId() {
+        System.out.println("------------ Поиск заявки по Id --------------");
+        String id = this.input.ask("Введите id заявки :");
+        Item item = this.tracker.findById(id);
+        System.out.println("Имя заявки: " + item.getName());
+        System.out.println("Описание заявки: " + item.getDesc());
+        System.out.println("Id заявки: " + item.getId());
+    }
+
+    private void findName() {
+        System.out.println("------------ Поиск заявки по имени --------------");
+        String name = this.input.ask("Введите имя заявки :");
+        Item[] items = tracker.findByName(name);
+        for (Item item : items) {
+            System.out.println("Имя заявки: " + item.getName());
+            System.out.println("Описание заявки: " + item.getDesc());
+            System.out.println("Id заявки: " + item.getId());
+            System.out.println("----------------------------------------------");
+        }
+    }
+
+    private void showMenu() {
+        System.out.println("Меню.");
+        System.out.println("0. Add new Item.");
+        System.out.println("1. Show all items.");
+        System.out.println("2. Edit item.");
+        System.out.println("3. Delete item.");
+        System.out.println("4. Find item by Id.");
+        System.out.println("5. Find items by name.");
+        System.out.println("6. Exit Program.");
+        // добавить остальные пункты меню.
+    }
+
+    /**
+     * Запускт программы.
+     * @param args
+     */
+    public static void main(String[] args) {
+        new StartUI(new ConsoleInput(), new Tracker()).init();
+    }
+}
