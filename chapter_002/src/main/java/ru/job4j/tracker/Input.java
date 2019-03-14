3f2f700dcac1c5fd1e8469464484b604e0d176ea
Mrdomax <kasian.job@gmail.com>
2019-03-14 14:27:17 +0300

    1. Обеспечить бесперебойную работу приложения Tracker.[#111805] Вариант 2

--- chapter_002/src/main/java/ru/job4j/tracker/Input.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Input.java
@@ -1,10 +1,8 @@
 package ru.job4j.tracker;
 
-import java.util.List;
-
 public interface Input {
     String ask(String question);
 
-    int ask(String question, List<Integer> range);
+    int ask(String question, int[] range);
 
 }
