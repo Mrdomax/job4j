31bf8322135c2824442bd6ad20eba6638b9d0fcd
Mrdomax <kasian.job@gmail.com>
2019-03-16 18:46:55 +0300

    1. Singleton.[#111812] Исправил тесты

--- chapter_002/src/test/java/ru/job4j/singl/SinglTest.java
+++ chapter_002/src/test/java/ru/job4j/singl/SinglTest.java
@@ -10,49 +10,33 @@ public class SinglTest {
     @Test
     public void TrackerFirstSingleCheck() {
         TrackerFirst tracker = TrackerFirst.INSTANCE;
-        Item frst = new Item("test1", "testDescription0", 123L);
-        tracker.add(frst);
         TrackerFirst tracker2 = TrackerFirst.INSTANCE;
-        Item scnd = new Item("test1", "testDescription1", 1234L);
-        tracker2.add(scnd);
-        Item[] expect = {frst, scnd};
-        assertThat(tracker.findAll(), is(expect));
+        boolean expect = true;
+        assertThat(tracker == tracker2, is(expect));
     }
 
     @Test
     public void TrackerSecondSingleCheck() {
         TrackerSecond tracker = TrackerSecond.getInstance();
-        Item frst = new Item("test1", "testDescription0", 123L);
-        tracker.add(frst);
         TrackerSecond tracker2 = TrackerSecond.getInstance();
-        Item scnd = new Item("test1", "testDescription1", 1234L);
-        tracker2.add(scnd);
-        Item[] expect = {frst, scnd};
-        assertThat(tracker.findAll(), is(expect));
+        boolean expect = true;
+        assertThat(tracker == tracker2, is(expect));
     }
 
     @Test
     public void TrackerThirdSingleCheck() {
         TrackerThird tracker = TrackerThird.getInstance();
-        Item frst = new Item("test1", "testDescription0", 123L);
-        tracker.add(frst);
         TrackerThird tracker2 = TrackerThird.getInstance();
-        Item scnd = new Item("test1", "testDescription1", 1234L);
-        tracker2.add(scnd);
-        Item[] expect = {frst, scnd};
-        assertThat(tracker.findAll(), is(expect));
+        boolean expect = true;
+        assertThat(tracker == tracker2, is(expect));
     }
 
     @Test
     public void TrackerFourthSingleCheck() {
         TrackerFourth tracker = TrackerFourth.getInstance();
-        Item frst = new Item("test1", "testDescription0", 123L);
-        tracker.add(frst);
         TrackerFourth tracker2 = TrackerFourth.getInstance();
-        Item scnd = new Item("test1", "testDescription1", 1234L);
-        tracker2.add(scnd);
-        Item[] expect = {frst, scnd};
-        assertThat(tracker.findAll(), is(expect));
+        boolean expect = true;
+        assertThat(tracker == tracker2, is(expect));
     }
 
 
